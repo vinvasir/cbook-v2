fe92d9ade056fbc641db7ca116dc099ec442c8e8
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-06 13:58:59 -0400

    wrote the url, view method, and template for the comic books index page

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from .models import Author, ComicBook
 # Create your views here.
+def index(request):
+    context = {'books': ComicBook.objects.all()}
+    return render(request, 'books/index.html', context)
