462446b68decf296cfc304449dc28133f3c61e76
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-06 22:10:23 -0400

    wrote the url and view method for creating comic books

--- books/forms.py
+++ books/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import ComicBook, Author
 
-class ComicBookForm(forms.From):
+class ComicBookForm(forms.Form):
     class Meta:
         model = ComicBook
         fields = ['title', 'description',]
-        labels = ['title': 'title', 'description': 'description',]
+        labels = {'title': 'title', 'description': 'description',}
--- books/views.py
+++ books/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Author, ComicBook
+from .forms import ComicBookForm
 # Create your views here.
 def index(request):
     context = {'books': ComicBook.objects.all()}
     return render(request, 'books/index.html', context)
+
+def new(request):
+    if request.method != 'POST':
+        form = ComicBookForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('books:index'))
+
+    context = {'form': form}
+    return render(request, 'books/new.html', context)
