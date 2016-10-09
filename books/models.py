07953b0a117375f65c9d91abaa61642b6dfd94f4
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-08 21:18:38 -0400

    created a Review model and migrated it

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=50)
@@ -16,3 +17,10 @@ class ComicBook(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    content = models.TextField()
+    rating = models.IntegerField()
+    comic_book = models.ForeignKey(ComicBook)
+    user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)    
