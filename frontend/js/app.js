7ea8cd60121ac1b9b4e7973cc5762909b8cceaa7
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-09 15:30:43 -0400

    created empty route and function for addign new books

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,12 +8,16 @@ angular
 			})
 			.state('books', {
 				url: '/books',
-				templateUrl: 'js/templates/books.html',
+				templateUrl: 'js/templates/books/list.html',
 				controller: 'BooksController as bookCtrl',
 				resolve: {
 					book: function($http, $stateParams) {
 						return $http.get('http://127.0.0.1:8000/books/json');
 					}
 				}
+			})
+			.state('books.new', {
+				url: '/books/new',
+				templateUrl: 'js/templates/books/new.html'
 			})
 	});
--- frontend/js/controllers/BooksController.js
+++ frontend/js/controllers/BooksController.js
@@ -2,6 +2,12 @@ function BooksController(book, $http){
 	var bookCtrl = this;
 
 	this.bookData = book.data;
+
+	this.newBook = {};
+
+	this.addBook = function(){
+
+	};
 }
 
 angular
