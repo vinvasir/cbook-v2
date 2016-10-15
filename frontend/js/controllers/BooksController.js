6d1189a17ac41e916e584c20aefb61522a748233
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-15 19:08:53 -0400

    changed the angular front end code for posting new books so that it successfully submits to the server

--- frontend/js/controllers/BooksController.js
+++ frontend/js/controllers/BooksController.js
@@ -6,8 +6,9 @@ function BooksController(book, $http, AuthorService){
 	this.newBook = {};
 
 	this.addBook = function(){
+		console.log(bookCtrl.newBook);
 		$http
-			.post('http://127.0.0.1:8000/books/json/', {comic_book: bookCtrl.newBook})
+			.post('http://127.0.0.1:8000/books/json/', bookCtrl.newBook)
 			.then(function(res){
 				console.log(res.data);
 				bookCtrl.data.push(res.data);
